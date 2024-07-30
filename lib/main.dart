83c9f924a0fe7f1d8ee563fe2afd73f0eb89f775
yogapraseetia <yogapraseetia@gmail.com>
2024-07-30 11:54:21 +0000

    perbaikan minor

--- lib/main.dart
+++ lib/main.dart
@@ -42,29 +42,36 @@ class _HtmlGridViewState extends State<HtmlGridView> {
   }
 
   Future<void> fetchAndParseHtml() async {
-    final url = 'https://tv4.lk21official.mom/populer/page/$_currentPage/';
-    final response = await http.get(Uri.parse(url));
     setState(() {
       _isLoading = true;
     });
 
+    final url = 'https://tv4.lk21official.mom/populer/page/$_currentPage/';
+    final response = await http.get(Uri.parse(url));
+
     if (response.statusCode == 200) {
       final document = parse(response.body);
-      final divs = document.querySelectorAll('div.col-lg-2.col-sm-3.col-xs-4.page-$_currentPage.infscroll-item');
+      final divs = document.querySelectorAll(
+          'div.col-lg-2.col-sm-3.col-xs-4.page-$_currentPage.infscroll-item');
 
       List<Map<String, String>> extractedItems = divs.map((div) {
         final articleElement = div.querySelector('article.mega-item');
-        final posterElement = articleElement?.querySelector('figure.grid-poster a');
+        final posterElement =
+            articleElement?.querySelector('figure.grid-poster a');
         final imageElement = posterElement?.querySelector('img');
-        final titleElement = articleElement?.querySelector('header.grid-header h1.grid-title a');
-        final ratingElement = articleElement?.querySelector('div.grid-meta div.rating');
-        final qualityElement = articleElement?.querySelector('div.grid-meta div.quality');
-        final durationElement = articleElement?.querySelector('div.grid-meta div.duration');
+        final titleElement =
+            articleElement?.querySelector('header.grid-header h1.grid-title a');
+        final ratingElement =
+            articleElement?.querySelector('div.grid-meta div.rating');
+        final qualityElement =
+            articleElement?.querySelector('div.grid-meta div.quality');
+        final durationElement =
+            articleElement?.querySelector('div.grid-meta div.duration');
 
         return {
           'title': titleElement?.text ?? '',
           'href': posterElement?.attributes['href'] ?? '',
-          'image': imageElement?.attributes['src'] ?? '',
+          'image': 'http:${imageElement!.attributes['src']}',
           'rating': ratingElement?.text ?? '',
           'quality': qualityElement?.text ?? '',
           'duration': durationElement?.text ?? '',
@@ -84,7 +91,9 @@ class _HtmlGridViewState extends State<HtmlGridView> {
   }
 
   void _scrollListener() {
-    if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent && !_isLoading) {
+    if (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent &&
+        !_isLoading) {
       _currentPage++;
       fetchAndParseHtml();
     }
@@ -97,47 +106,71 @@ class _HtmlGridViewState extends State<HtmlGridView> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HTML Grid View'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GridView.builder(
-              controller: _scrollController,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-              ),
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => MoviesDetail(item['href'] ?? '')));
-                  },
-                  child: Card(
-                    child: Column(
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('HTML Grid View'),
+    ),
+    body: Column(
+      children: [
+        Expanded(
+          child: GridView.builder(
+            controller: _scrollController,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 5,
+              childAspectRatio: 3 / 4,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemCount: items.length + (_isLoading ? 1 : 0), // Add one for loading indicator if needed
+            itemBuilder: (context, index) {
+              if (index >= items.length) {
+                // If it's the last item, show a loading indicator
+                return const Center(child: CircularProgressIndicator());
+              }
+              final item = items[index];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MoviesDetail(item['href'] ?? '')));
+                },
+                child: Card(
+                  elevation: 5,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Stack(
                       children: [
-                        Text(item['title'] ?? ''),
-                        Text(item['rating'] ?? ''),
-                        Text(item['quality'] ?? ''),
-                        Text(item['duration'] ?? ''),
+                        Image.network(item['image'] ?? '',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: double.infinity),
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            color: Colors.black.withOpacity(0.5),
+                            child: Text(item['title'] ?? '',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                overflow: TextOverflow.ellipsis),
+                          ),
+                        ),
                       ],
                     ),
                   ),
-                );
-              },
-            ),
+                ),
+              );
+            },
           ),
-          if (_isLoading)
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: CircularProgressIndicator(),
-            ),
-        ],
-      ),
-    );
-  }
+        ),
+      ],
+    ),
+  );
+}
 }
